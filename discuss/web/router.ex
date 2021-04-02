9639742bdd1367272d241d67472238326aecdfb3
Christian Blanco <christiane.gb@hotmail.com>
2021-04-02 10:40:42 -0600

    Updateing more things on phoenix

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -16,9 +16,14 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    # We can use each route or we can use the "resources" helper
+    # following the REST standards.
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
