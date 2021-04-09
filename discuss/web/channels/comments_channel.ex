a65e0e1b5bf06e33631e29170bac44c246eeec13
Christian Blanco <christiane.gb@hotmail.com>
2021-04-09 00:49:01 -0500

    Adding first views and events for channels and sockets in phoenix

---
+++ discuss/web/channels/comments_channel.ex
@@ -0,0 +1,15 @@
+defmodule Discuss.CommentsChannel do
+	use Discuss.Web, :channel
+
+	def join(name, _params, socket) do
+		{:ok, %{hey: "there"}, socket}
+	end
+
+	def handle_in(name, message, socket) do
+		IO.puts("++++++")
+		IO.puts(name)
+		IO.inspect(message)
+		{:reply, :ok, socket}
+	end
+
+end
