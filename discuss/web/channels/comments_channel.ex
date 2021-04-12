1f6aa0d21f59c9e74b753446756e128fe80a9a36
chrisegb <christiane.gb@hotmail.com>
2021-04-12 18:16:28 -0500

    Finishing the discuss platform

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -7,19 +7,25 @@ defmodule Discuss.CommentsChannel do
 
 		topic = Topic
 			|> Repo.get(topic_id)
-			|> Repo.preload(:comments)
+			|> Repo.preload(comments: [:user])
 
 		{:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
 	end
 
 	def handle_in(name, %{"content" => content}, socket) do
 		topic = socket.assigns.topic
+		user_id = socket.assigns.user_id
 		changeset = topic
-			|> build_assoc(:comments)
+			|> build_assoc(:comments, user_id: user_id)
 			|> Comment.changeset(%{content: content})
 
 		case Repo.insert(changeset) do
 			{:ok, comment} ->
+				broadcast!(
+					socket, 
+					"comments:#{socket.assigns.topic.id}:new", 
+					%{comment: comment}
+				)
 				{:reply, :ok, socket}
 			{:error, _reason} ->
 				{:reply, {:error, %{errors: changeset}}, socket}
--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -6,16 +6,53 @@ socket.connect()
 
 const createSocket = (topicId) => {
 	let channel = socket.channel(`comments:${topicId}`, {});
-	channel.join()
-	  .receive("ok", resp => { console.log("Joined successfully", resp) })
-	  .receive("error", resp => { console.log("Unable to join", resp) });
-
-	
+	channel
+	.join()
+	.receive("ok", resp => { 
+		console.log(resp);
+		renderComments(resp.comments);
+	})
+	.receive("error", resp => { 
+		console.log("Unable to join", resp) 
+	});
 	document.querySelector("button").addEventListener("click", function(){
 		const content = document.querySelector("textarea").value;
 		channel.push("comment:add", {content: content});
 	});
-	
+
+	channel.on(`comments:${topicId}:new`, renderComment);
+}
+
+function renderComments(comments) {
+	const renderedComments = comments.map(comment => {
+		return commentTemplate(comment);
+	});
+
+	document.querySelector(".collection").innerHTML = renderedComments.join('');
+}
+
+function renderComment(event) {
+	const renderedComment = commentTemplate(event.comment);
+
+	document.querySelector(".collection").innerHTML += renderedComment;	
+}
+
+function commentTemplate(comment) {
+
+	let email = "Anonymous";
+
+	if (comment.user) {
+		email = comment.user.email;
+	}
+
+	return `
+		<li class="collection-item">
+			${comment.content}
+			<div class="secondary-content">
+				${email}
+			</div>
+		</li>
+	`;
 }
 
 window.createSocket = createSocket;
